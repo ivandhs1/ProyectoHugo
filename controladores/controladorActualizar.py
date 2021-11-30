648156387f37f030d4f71220f74ae8dd63d942a6
ivan <ivanhshs1@gmail.com>
2021-11-30 13:59:10 -0500

    commit pasando main

--- controladores/controladorActualizar.py
+++ controladores/controladorActualizar.py
@@ -2,16 +2,9 @@
 from bd import Obtener_Conexion
 
 
-def ActualizarDireccion(documento,direccion):
+def actualizarCliente(documento,movil,direccion):
     conexion = Obtener_Conexion()
     with conexion.cursor() as cursor:
-        cursor.execute("UPDATE clientes SET direccion = %s WHERE documento = %s",(direccion, documento))
-    conexion.commit()
-    conexion.close()
-
-def ActualizaMovil(documento,movil):
-    conexion = Obtener_Conexion()
-    with conexion.cursor() as cursor:
-        cursor.execute("UPDATE clientes SET movil = %s WHERE documento = %s",(movil, documento))
+        cursor.execute("UPDATE clientes SET direccion = %s movil = %s WHERE documento = %s",(direccion, movil, documento))
     conexion.commit()
     conexion.close()
