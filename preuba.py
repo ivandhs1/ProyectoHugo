08077469a37797edfb2ca8ea7d55da72dbc10ba2
ivan <ivanhshs1@gmail.com>
2021-12-01 13:23:42 -0500

    commit terminando actualizar saldo

--- preuba.py
+++
@@ -1,9 +0,0 @@
-deudaActual=5000
-deudaActual=deudaActual-10000
-
-if deudaActual<0:
-    print("menor a cero",deudaActual)
-    deudaActual=deudaActual*(-1)
-    print (deudaActual)
-elif deudaActual>0:
-    print(deudaActual)
