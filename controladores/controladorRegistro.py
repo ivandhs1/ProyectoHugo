f805708ca508d3370092808aaeab3540694db17f
Cristian Amador <crissis2004@gmail.com>
2021-12-01 14:49:59 -0500

    responsive 3

--- controladores/controladorRegistro.py
+++ controladores/controladorRegistro.py
@@ -3,7 +3,7 @@ from bd import Obtener_Conexion
 def RegistrarCliente(documento, nombre, movil, direccion, deuda, aFavor):
     conexion = Obtener_Conexion()
     with conexion.cursor() as cursor:
-        cursor.execute ("INSERT INTO clientes(documento,nombre,movil,direccion,deuda, aFavor) VALUES (%s,%s,%s,%s,%s,%s) ",(documento,nombre,movil,direccion,deuda, aFavor))
+        cursor.execute ("INSERT INTO clientes(documento,nombre,movil,direccion,deuda, a_favor) VALUES (%s,%s,%s,%s,%s,%s) ",(documento,nombre,movil,direccion,deuda, aFavor))
     conexion.commit()
     conexion.close()
     
