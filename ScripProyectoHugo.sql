6929439a12c879280f8104d3e67e4264008e7910
Brayni Campo <braynivalentina@gmail.com>
2021-12-10 00:34:44 -0500

    commit bd

--- ScripProyectoHugo.sql
+++ ScripProyectoHugo.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA bd_proyectohugo;
 use bd_proyectohugo;
 
 CREATE TABLE clientes
-( 	documento int Primary Key not null,
+( 	documento bigint Primary Key not null,
 	nombre char(70),
     movil char(20),
     direccion char(50),
